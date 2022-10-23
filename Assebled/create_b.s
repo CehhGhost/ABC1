f1a31a054b4e3520de95b61ea14f9d9a6d11fc06
CehhGhost <71556063+CehhGhost@users.noreply.github.com>
2022-10-23 15:33:55 +0300

    Add files via upload

---
+++ Assebled/create_b.s
@@ -0,0 +1,91 @@
+	.file	"create_b.c"
+	.intel_syntax noprefix
+	.text
+	.globl	create_b
+	.type	create_b, @function
+create_b:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	mov	QWORD PTR -24[rbp], rdi	# указатель на A
+	mov	QWORD PTR -32[rbp], rsi	# указатель на B
+	mov	DWORD PTR -36[rbp], edx	# указатель на size
+	mov	DWORD PTR -12[rbp], 0	# i (= -12)
+	mov	DWORD PTR -8[rbp], 0	# j (= -8)
+	mov	eax, DWORD PTR -36[rbp]
+	mov	edx, eax
+	shr	edx, 31
+	add	eax, edx
+	sar	eax
+	mov	DWORD PTR -4[rbp], eax	# k (= -4)
+	jmp	.L2
+.L5:
+	mov	eax, DWORD PTR -12[rbp]
+	and	eax, 1
+	test	eax, eax
+	jne	.L3
+	mov	eax, DWORD PTR -12[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*4]
+	mov	rax, QWORD PTR -24[rbp]
+	add	rax, rdx
+	mov	edx, DWORD PTR -4[rbp]
+	movsx	rdx, edx
+	lea	rcx, 0[0+rdx*4]
+	mov	rdx, QWORD PTR -32[rbp]
+	add	rdx, rcx
+	mov	eax, DWORD PTR [rax]
+	mov	DWORD PTR [rdx], eax
+	add	DWORD PTR -4[rbp], 1
+	jmp	.L4
+.L3:
+	mov	eax, DWORD PTR -12[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*4]
+	mov	rax, QWORD PTR -24[rbp]
+	add	rax, rdx
+	mov	edx, DWORD PTR -8[rbp]
+	movsx	rdx, edx
+	lea	rcx, 0[0+rdx*4]
+	mov	rdx, QWORD PTR -32[rbp]
+	add	rdx, rcx
+	mov	eax, DWORD PTR [rax]
+	mov	DWORD PTR [rdx], eax
+	add	DWORD PTR -8[rbp], 1
+.L4:
+	add	DWORD PTR -12[rbp], 1
+.L2:
+	mov	eax, DWORD PTR -12[rbp]
+	cmp	eax, DWORD PTR -36[rbp]
+	jl	.L5
+	nop
+	nop
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	create_b, .-create_b
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
---
+++ Assebled/print_mass.s
@@ -0,0 +1,69 @@
+	.file	"print_mass.c"
+	.intel_syntax noprefix
+	.text
+	.section	.rodata
+.LC0:
+	.string	"B[%d] = %d\r\n"
+.LC1:
+	.string	"\r"
+	.text
+	.globl	print_mass
+	.type	print_mass, @function
+print_mass:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	sub	rsp, 32
+	mov	QWORD PTR -24[rbp], rdi	# int* mass (= -24)
+	mov	DWORD PTR -28[rbp], esi	# int size (= -28)
+	mov	DWORD PTR -4[rbp], 0	# int size (= -4)
+	jmp	.L2
+.L3:
+	mov	eax, DWORD PTR -4[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*4]
+	mov	rax, QWORD PTR -24[rbp]
+	add	rax, rdx
+	mov	edx, DWORD PTR [rax]
+	mov	eax, DWORD PTR -4[rbp]
+	mov	esi, eax
+	lea	rdi, .LC0[rip]
+	mov	eax, 0
+	call	printf@PLT
+	add	DWORD PTR -4[rbp], 1
+.L2:
+	mov	eax, DWORD PTR -4[rbp]
+	cmp	eax, DWORD PTR -28[rbp]
+	jl	.L3
+	lea	rdi, .LC1[rip]
+	call	puts@PLT
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	print_mass, .-print_mass
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
